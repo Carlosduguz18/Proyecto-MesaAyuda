7d25b4afc64c7ea0093a2ecdb82bd0beeba33890
Cardelman18 <carlosguzman180300@gmail.com>
2023-06-01 12:31:45 -0500

    Actualizacion 01

--- Proyecto Ing Software/js/tickets.js
+++ Proyecto Ing Software/js/tickets.js
@@ -3,16 +3,38 @@
 window.addEventListener('DOMContentLoaded', function() {
     // Obtener los parámetros de URL
     // var urlParams = new URLSearchParams(window.location.search);
-    var nameUser = "Carlos Dueñas"
-    var nameProject = "Odoo"
+    var affair = "Deseo realizar un mantenimiento en la base de datos"
+    var nameTable = "Backup"
+    var dateLine = "01/06/2023"
+    var client = "Carlos Dueñas"
+    var nameProject = "Mantenimiento de la base de datos"
+    var priority = "Urgente"
+    var description = "Se cayó la base de datos por cierta cuestión que pasó .."
+
   
-    if (nameUser && nameProject) {
+    if (affair && nameTable && dateLine && client && nameProject && priority && description) {
       // Crear un card con los datos del formulario
       var ticket = document.createElement('div')
       ticket.classList.add('ticket')
       ticket.innerHTML = `
-        <h3>${nameUser}</h3>
-        <h4>Proyecto: ${nameProject}</h4>
+        <label for="asuntoInput">${affair}</label> <br><br>
+      
+        <label for="tablaInput">${nameTable}</label> <br><br>
+      
+        <label for="fechaInput">Fecha de Solicitud</label>
+        <label for="fechaInput">${dateLine}</label><br><br>
+      
+        <label for="clienteInput">Cliente</label>
+        <label for="clienteInput">${client}</label><br><br>
+      
+        <label for="proyectoInput">Proyecto</label>
+        <label for="proyectoInput">${nameProject}</label><br><br>
+      
+        <label for="prioridadInput">Prioridad</label>
+        <label for="prioridadInput">${priority}</label><br><br>
+      
+        <label for="descripcionInput">Descripción del ticket</label>
+        <textarea id="descripcionInput" placeholder="Descripción del ticket">${description}</textarea>
       `
   
       // Agregar el card a la primera tabla
